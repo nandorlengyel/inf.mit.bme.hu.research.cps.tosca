dc647e884959afce70ab8e55abd6c9bddddb8640
nandorlengyel <lengyel.nandi@gmail.com>
2021-05-09 14:43:45 +0200

    Remove output folder

--- inf.mit.bme.hu.research.cps.tosca.example/output/dds_application_2/dds_topic_4_publisher.py
+++
@@ -1,7 +0,0 @@
-from dds_threads import WriterThread
-
-class dds_topic_4Writer(WriterThread):
-
-	def produce_data(self, output):
-		print(self.topic_name + " data sent")
-		
--- inf.mit.bme.hu.research.cps.tosca.example/output/event_based_dds_application_7/dds_topic_8_subscriber.py
+++
@@ -1,8 +0,0 @@
-from dds_threads import ReaderThread
-
-class dds_topic_8Reader(ReaderThread):
-
-	def process_data(self, sample):
-		print(self.topic_name + " data received: ")
-		print(sample.get_dictionary())
-
